898440790d1b11150f2d1629f1e2aafe56fcbace
Fahiyan Shah Chand <fahiyanofficial@gmail.com>
2024-09-27 15:56:22 +0600

    Create 16. Counting Digits.cpp

---
+++ 16. Counting Digits.cpp
@@ -0,0 +1,23 @@
+// A program to count the number of digits in an integer using a while loop.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void calculate(int n){
+    int digits = 0;
+    while(n > 0){
+        digits++;
+        n /= 10;
+    }
+    cout << "The number of digits: " << digits << endl;
+}
+
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    calculate(n);
+    return 0;
+}
